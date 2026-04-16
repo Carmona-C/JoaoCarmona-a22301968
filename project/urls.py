e072bb67d4306313e810197305a59ca5d92dae25
Joao Carmona a22301968 <126264472+Carmona-C@users.noreply.github.com>
2026-04-16 17:02:04 +0000

    criacaoEscolas

--- project/urls.py
+++ project/urls.py
@@ -16,13 +16,23 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include              #     <- adicionar include 
 from django.conf import settings
 from django.conf.urls.static import static
 
+## project/urls.py
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("escola/", include("escola.urls")), 
+    path("", include("escola.urls")),  #  rota para app escola sem precisar de escrever "escola"
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+## ficheiro projects/urls.py
+
+
+
+
